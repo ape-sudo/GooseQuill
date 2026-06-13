cf810fc4037e917fea614b26779ee2a35a5d0f8e
ape-sudo <jakuboczkowski2@gmail.com>
2026-06-13 20:06:50 +0200

    Cleanup build

--- build.zig
+++ build.zig
@@ -3,42 +3,26 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const mod = b.addModule("GooseQuill", .{
+
+    const gq_mod = b.addModule("GooseQuill", .{
         .root_source_file = b.path("src/root.zig"),
-       // which requires us to specify a target.
         .target = target,
+    });
+
+    const root_module = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+        .imports = &.{.{
+            .name = "GooseQuill",
+            .module = gq_mod,
+        }},
     });
 
     const exe = b.addExecutable(.{
         .name = "GooseQuill",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-            .imports = &.{
-            },
-        }),
+        .root_module = root_module,
     });
 
     b.installArtifact(exe);
-    const run_step = b.step("run", "Run the app");
-    const run_cmd = b.addRunArtifact(exe);
-    run_step.dependOn(&run_cmd.step);
-
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const mod_tests = b.addTest(.{
-        .root_module = mod,
-    });
-
-    const run_mod_tests = b.addRunArtifact(mod_tests);
-
-    const exe_tests = b.addTest(.{
-        .root_module = exe.root_module,
-    });
-
 }
